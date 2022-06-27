767e59a8b7a2a78ea56dec54bfb557610af841ef
SHERBEK <mavlonovsherbek43@gmail.com>
2022-06-27 09:53:18 +0500

    carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import CustomCarousel from './components/carousel'
 import Header from './components/header'
 
 const App: React.FC = () => {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <CustomCarousel />
+    </>
+  )
 }
 
 export default App
